10b62019f2ac8f8a626786fc6f3056f24338e603
AugustoM94 <email@github.com>
2023-10-17 16:38:30 +0200

    added first exercise on js and html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+ //utility
+  const parolaInput = document.getElementById("parolaInput");
+  const verifica = document.getElementById("verifica");
+  const risultato = document.getElementById("risultato");
+
+ verifica.addEventListener("click", function () {
+  //variabili
+
+    const parola = parolaInput.value.toUpperCase;    
+    let isPalindromo = true;
+//ciclo for 
+
+    for (let i = 0; i < parola.length / 2; i++) {
+      console.log(parola)
+      if (parola[i] !== parola[parola.length - 1 - i]) {
+        isPalindromo = false;
+      }
+    }
+
+    if (isPalindromo) {
+      risultato.textContent = "La parola è un palindromo";
+    } else {
+      risultato.textContent = "La parola non è un palindromo";
+    }
+  });
 
